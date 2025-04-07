ff7d6cf58cb978ea70e4bb71c43f41b6e6b4829a
Rushik-B <104290038+Rushik-B@users.noreply.github.com>
2025-04-07 10:44:49 -0700

    Update PaperDisplay.jsx

--- src/frontend/PaperDisplay.jsx
+++ src/frontend/PaperDisplay.jsx
@@ -93,7 +93,7 @@ const PaperDisplay = ({ claim }) => {
       progressThroughStages();
       
       try {
-        const response = await fetch(`http://127.0.0.1:8080/api/verify_claim`, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL || 'https://final-project-15-mountains-production.up.railway.app'}/api/verify_claim`, 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
