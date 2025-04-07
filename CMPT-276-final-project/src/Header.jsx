2559d3d7aebb3feb5ee93aae2e0b5c7e7facfe54
Rushik-B <rushikbusiness@gmail.com>
2025-04-07 09:31:25 -0700

    Made changes to the front-end and cleaned up the files

--- CMPT-276-final-project/src/Header.jsx
+++ CMPT-276-final-project/src/Header.jsx
@@ -16,9 +16,6 @@ export default function Header() {
             </div>
             <nav className="nav">
               <ul className="nav-list">
-                <li><a href="#features">Features</a></li>
-                <li><a href="#pricing">Pricing</a></li>
-                <li><Link to="/contact">Contact</Link></li>
               </ul>
             </nav>
          
@@ -29,13 +26,9 @@ export default function Header() {
                   <path d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
               </button>
-              <button className="sign-in-button">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                  <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                Sign In
-              </button>
+              <Link to="/contact" className="search-button contact-button">
+                Contact
+              </Link>
             </div>
           </div>
         </header>
